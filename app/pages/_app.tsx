8bc8ef36e61902f3a6f70852031001fbe709b5cc
msmaiaa <roxx77344@gmail.com>
2021-08-31 06:33:18 -0300

    fix material bug

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,17 @@
 import './styles.css'
 import 'react-toastify/dist/ReactToastify.css';
-import ToastContext, { success, error, warn } from '../context/ToastContext'
 import { ToastContainer } from 'react-toastify'
 
+import { useEffect } from 'react';
+import ToastContext, { success, error, warn } from '../context/ToastContext'
+
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, [])
   return <>
   <ToastContext.Provider value={{success, error, warn}}>
     <Component {...pageProps} />
